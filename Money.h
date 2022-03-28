5be335ddeea3ec62344cad79be8066be3e122746
daley-evan <edaley@nevada.unr.edu>
2022-03-27 20:58:50 -0700

    updated code with errors

--- Money.h
+++ Money.h
@@ -12,11 +12,16 @@ class Money {
     private:
         int dollars;
         int cents;
+        int currentBalance;
         std::vector<int> all_cents;
 
     public:
         Money();
         explicit Money(const int &m_dollars, const int &m_cents);
+
+        //not sure if needed
+        //void setcurrentBalance(int currentBalance);
+        //int getcurrentBalance() const;
 
         bool operator< (const Money &rhs) const {
 		    return all_cents < rhs.all_cents;
@@ -43,25 +48,31 @@ class Money {
             return all_cents == rhs.all_cents;
         }
 
-        Money operator+ (const Money &rhs) {
-            return *this;
-        }
+        //Needs fixing
+        //Money operator+= (const Money &rhs) {
+            //return all_cents += rhs.all_cents;
+        //}
 
-        Money operator- (const Money &rhs) {
-            return *this;
-        }
+        //Money operator- (const Money &rhs) { 
+            //return *this;
+        //}
+
+        //Money operator*(const Money &rhs) {
+            //return all_cents * rhs.all_cents;
+        //}    
         
-        friend std::ostream &operator << (std::ostream &os, const Money &money) {
+        friend std::ostream &operator<<(std::ostream &os, const Money &money) {
             for (auto i : money.all_cents) {
                 std::cout << std::fixed;
                 os << "$" << std::setprecision(2) << i*0.01;
 	            return os;
-            }
-	            
+            } 
         }   
+        
 
         virtual ~Money();
 };
+
 
 
 #endif
